05fe92653c2762521d62fb7e1d55240c9245b306
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-07 16:30:39 +0530

    create a Student page UI

---
+++ src/main/java/lk/ijse/orm_coursework/controller/StudentPageController.java
@@ -0,0 +1,42 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class StudentPageController {
+    public Label lblCustomerId;
+    public TextField txtFirstName;
+    public TextField txtLastName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtDOB;
+    public TextField txtRegDate;
+    
+    public Button btnSave;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public Button btnClean;
+    
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colFirstName;
+    public TableColumn colLastName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colDOB;
+    public TableColumn colRegDate;
+
+    public void btnSaveOncAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpadateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCleanOnAction(ActionEvent actionEvent) {
+    }
+}
