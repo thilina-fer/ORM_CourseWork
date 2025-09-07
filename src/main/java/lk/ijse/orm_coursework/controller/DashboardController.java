762dc086b1257a8ba1ca0750cfef7778e8c2e4c1
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-07 22:03:24 +0530

    create a DashboardController

---
+++ src/main/java/lk/ijse/orm_coursework/controller/DashboardController.java
@@ -0,0 +1,39 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+
+
+public class DashboardController {
+
+    public AnchorPane ancDashboard;
+
+
+
+    void navigateTo(String path) {
+        try {
+            ancDashboard.getChildren().clear();
+
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource(path));
+
+            anchorPane.prefWidthProperty().bind(ancDashboard.widthProperty());
+            anchorPane.prefHeightProperty().bind(ancDashboard.heightProperty());
+
+            ancDashboard.getChildren().add(anchorPane);
+
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void btnStudentOnAction(ActionEvent actionEvent) {
+        navigateTo("/view/StudentPage.fxml");
+    }
+
+    public void btmInstructorOnAction(ActionEvent actionEvent) {
+        navigateTo("/view/InstructorPage.fxml");
+    }
+}
