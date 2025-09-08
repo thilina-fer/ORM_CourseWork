581338c2a5638b84c7a52760d4e5faf6acd39937
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-08 07:12:59 +0530

    create a userDTO class

---
+++ src/main/java/lk/ijse/orm_coursework/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.orm_coursework.dto;
+
+public class UserDTO {
+    private String userId;
+    private String username;
+    private String password;
+    private String role;
+    private String email;
+}
