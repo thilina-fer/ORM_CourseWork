882163cb06a2329c36f5f1f6109cb589f5a66206
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-08 09:31:59 +0530

    modify a BOFactory

--- src/main/java/lk/ijse/orm_coursework/bo/BOFactory.java
+++ src/main/java/lk/ijse/orm_coursework/bo/BOFactory.java
@@ -2,6 +2,7 @@ package lk.ijse.orm_coursework.bo;
 
 import lk.ijse.orm_coursework.bo.custom.impl.InstructorBOImpl;
 import lk.ijse.orm_coursework.bo.custom.impl.StudentBOImpl;
+import lk.ijse.orm_coursework.bo.custom.impl.UserBOImpl;
 
 public class BOFactory {
     private static BOFactory boFactory;
@@ -17,6 +18,7 @@ public class BOFactory {
         return switch (boType) {
             case STUDENT -> (Hello) new StudentBOImpl();
             case  INSTRUCTOR -> (Hello) new InstructorBOImpl();
+            case USER -> (Hello) new UserBOImpl();
         };
     }
 
--- src/main/java/lk/ijse/orm_coursework/bo/BOTypes.java
+++ src/main/java/lk/ijse/orm_coursework/bo/BOTypes.java
@@ -1,5 +1,5 @@
 package lk.ijse.orm_coursework.bo;
 
 public enum BOTypes {
-    STUDENT,INSTRUCTOR
+    STUDENT,INSTRUCTOR,USER
 }
