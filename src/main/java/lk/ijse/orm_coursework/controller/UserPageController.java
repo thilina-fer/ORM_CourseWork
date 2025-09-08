705d2cb7c9c01e5062dccb31aca50655d2a85ece
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-08 09:56:08 +0530

    create a User page UI

---
+++ src/main/java/lk/ijse/orm_coursework/controller/UserPageController.java
@@ -0,0 +1,40 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+public class UserPageController {
+
+    public Label lblUserId;
+    public TextField txtUsername;
+    public TextField txtPassword;
+    public TextField txtRole;
+    public TextField txtEmail;
+
+    public Button btnSave;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public Button btnClean;
+    public TableView tblUser;
+    public TableColumn colId;
+    public TableColumn colUsername;
+    public TableColumn colPassword;
+    public TableColumn colRole;
+    public TableColumn colEmail;
+
+    public void btnSaveOncAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCleanOnAction(ActionEvent actionEvent) {
+    }
+
+    public void onClickTable(MouseEvent mouseEvent) {
+    }
+}
