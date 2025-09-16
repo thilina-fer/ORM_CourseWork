7cd2a953186574e06d30a027f407db515fb66c46
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-16 18:53:04 +0530

    create a PaymentsDAO  interface

---
+++ src/main/java/lk/ijse/orm_coursework/dao/custom/PaymentDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.orm_coursework.dao.custom;
+
+import lk.ijse.orm_coursework.dao.CrudDAO;
+import lk.ijse.orm_coursework.entity.Payments;
+
+public interface PaymentDAO extends CrudDAO<Payments> {
+}
