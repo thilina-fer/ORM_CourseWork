f71a84de35e40dcdb929a893822a1e7d9ebd0bc0
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-16 18:44:40 +0530

    create a CourseDTO class

---
+++ src/main/java/lk/ijse/orm_coursework/dto/CourseDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.orm_coursework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+public class CourseDTO {
+    private String course_id;
+    private String course_name;
+    private String duration;
+    private double fee;
+    private String description;
+    private String instructor_id;
+    @Builder.Default
+    private ArrayList<StudentCourseDetailsDTO> studentCourseDetails = new ArrayList<>();
+    @Builder.Default
+    private ArrayList<LessonsDTO> lessons = new ArrayList<>();
+}
