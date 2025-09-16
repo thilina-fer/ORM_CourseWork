bb543092fdcf18342c6cdd2d71662d8c24cc2bf8
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-16 18:55:16 +0530

    create a CourseDAO  interface

--- src/main/java/lk/ijse/orm_coursework/dao/custom/InstructorDAO.java
+++ src/main/java/lk/ijse/orm_coursework/dao/custom/InstructorDAO.java
@@ -1,8 +1,8 @@
 package lk.ijse.orm_coursework.dao.custom;
 
 import lk.ijse.orm_coursework.dao.CrudDAO;
-import lk.ijse.orm_coursework.entity.Instructor;
+import lk.ijse.orm_coursework.entity.Instructors;
 
-public interface InstructorDAO extends CrudDAO<Instructor> {
+public interface InstructorDAO extends CrudDAO<Instructors> {
 
 }
