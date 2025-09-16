0b6f978a420fb34b261d7e03afd961739bde34e1
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-16 18:30:06 +0530

    create a PaymentsDTO class

---
+++ src/main/java/lk/ijse/orm_coursework/dto/PaymentsDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.orm_coursework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class PaymentsDTO {
+    private String paymentId;
+    private Date paymentDate;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    private String studentId;
+}
