b90e6eb7e55836b2c7eb57c8294a7ea394e94333
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-17 06:17:19 +0530

    create a LessonsBO interface

---
+++ src/main/java/lk/ijse/orm_coursework/bo/custom/LessonsBO.java
@@ -0,0 +1,22 @@
+package lk.ijse.orm_coursework.bo.custom;
+
+import lk.ijse.orm_coursework.dto.LessonsDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LessonsBO {
+    List<LessonsDTO> getAllLessons() throws Exception;
+
+    String getLastLessonId() throws Exception;
+
+    boolean saveLessons(LessonsDTO t) throws Exception;
+
+    boolean updateLessons(LessonsDTO t) throws Exception;
+
+    boolean deleteLessons(String id) throws Exception;
+
+    List<String> getAllLessonIds() throws Exception;
+
+    Optional<LessonsDTO> findByLessonId(String id) throws Exception;
+}
