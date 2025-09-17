8d8fca1ff2ef9387cd2b507bb98240f6ae300293
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-17 19:00:02 +0530

    create a InstructorController class

---
+++ src/main/java/lk/ijse/orm_coursework/controller/InstructorManagePageController.java
@@ -0,0 +1,25 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class InstructorManagePageController {
+    public Button btnAdd;
+    public TableView tblInstructor;
+    public TableColumn colId;
+    public TableColumn colFirstName;
+    public TableColumn colLastName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colSpecialization;
+    public TableColumn colAvailability;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void onClickTable(MouseEvent mouseEvent) {
+    }
+}
