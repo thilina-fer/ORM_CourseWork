c284dbe8b0e315fb7b09b211fead8f25e162c807
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-17 19:14:36 +0530

    create a UserPage UI and controller

---
+++ src/main/java/lk/ijse/orm_coursework/controller/UserPOPPageController.java
@@ -0,0 +1,23 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UserPOPPageController {
+
+    public Label lblId;
+    public TextField txtUserName;
+    public TextField txtPassword;
+    public TextField txtRole;
+    public TextField txtEmail;
+    public Button btnSave;
+    public Button btnUpdate;
+
+    public void btnSaveOncAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
