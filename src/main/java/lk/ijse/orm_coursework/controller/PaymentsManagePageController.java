18ac7321048d897e4cf9d2381d71088c2b1810d1
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-17 18:44:43 +0530

    create a paymentsManageController class

---
+++ src/main/java/lk/ijse/orm_coursework/controller/PaymentsManagePageController.java
@@ -0,0 +1,25 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class PaymentsManagePageController {
+    public TableView tblStudent;
+    public TableColumn colPaymentId;
+    public TableColumn colPaymentDate;
+    public TableColumn colAmount;
+    public TableColumn colPaymentMethod;
+    public TableColumn colStatus;
+    public TableColumn colStudentId;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAAction(ActionEvent actionEvent) {
+    }
+
+    public void onClickTable(MouseEvent mouseEvent) {
+    }
+}
