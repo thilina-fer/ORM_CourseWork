c3c24226ffe73b04eae749bc0fb8441a2dd70bad
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-17 18:54:00 +0530

    create a paymentsManagePOPController class

---
+++ src/main/java/lk/ijse/orm_coursework/controller/PaymentPOPPageController.java
@@ -0,0 +1,25 @@
+package lk.ijse.orm_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PaymentPOPPageController {
+
+    public TextField txtPaymentDate;
+    public TextField txtAmount;
+    public ComboBox cmbPaymentMethod;
+    public TextField txtStatus;
+    public TextField txtStudentId;
+    public Label lblId;
+    public Button btnSave;
+    public Button btnUpdate;
+
+    public void btnSaveOncAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
