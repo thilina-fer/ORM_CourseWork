8d347e46ed8cfff55d1aa0fc7497838b50285d6e
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-21 09:24:12 +0530

    create a course manage page controller

--- src/main/java/lk/ijse/orm_coursework/controller/CourseManegePageController.java
+++ src/main/java/lk/ijse/orm_coursework/controller/CourseManegePageController.java
@@ -1,13 +1,21 @@
 package lk.ijse.orm_coursework.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import lk.ijse.orm_coursework.bo.BOFactory;
 import lk.ijse.orm_coursework.bo.BOTypes;
 import lk.ijse.orm_coursework.bo.custom.CourseBO;
 import lk.ijse.orm_coursework.dto.tm.CourseTM;
+
+import java.io.IOException;
 
 public class CourseManegePageController {
     public TableView<CourseTM> tblStudent;
@@ -22,8 +30,21 @@ public class CourseManegePageController {
     private final CourseBO courseBO = (CourseBO) BOFactory.getInstance().getBO(BOTypes.COURSE);
 
     public void btnAddOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/CoursePOPPage.fxml"));
+            Parent parent = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add Course");
+            stage.setScene(new Scene(parent));
+            stage.initModality(Modality.APPLICATION_MODAL); // Block input to other windows
+            stage.showAndWait();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to open the popup!").show();
+        }
     }
 
     public void onClickTable(MouseEvent mouseEvent) {
+
     }
 }
