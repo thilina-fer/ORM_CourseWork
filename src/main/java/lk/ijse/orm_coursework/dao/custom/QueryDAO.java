1c6d3d69e306938c33dd6c1d11533a4a134da63c
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-24 16:08:08 +0530

    update quearyDAO class

--- src/main/java/lk/ijse/orm_coursework/dao/custom/QueryDAO.java
+++ src/main/java/lk/ijse/orm_coursework/dao/custom/QueryDAO.java
@@ -4,4 +4,5 @@ import lk.ijse.orm_coursework.dao.SuperDAO;
 
 public interface QueryDAO extends SuperDAO {
     public int getStudentCountForLesson(String lessonId);
+    double getTotalCourseAmountByStudentId(String studentId) throws Exception;
 }
