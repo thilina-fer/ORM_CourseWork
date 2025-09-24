ce8f485ca88726e578db24c76e099207d6e8aff7
thilina-fer <dilshanthilina1010@gmail.com>
2025-09-24 15:35:59 +0530

    create payments tm class

---
+++ src/main/java/lk/ijse/orm_coursework/dto/tm/PaymentsTM.java
@@ -0,0 +1,12 @@
+package lk.ijse.orm_coursework.dto.tm;
+
+import java.util.Date;
+
+public class PaymentsTM {
+    private String paymentId;
+    private Date paymentDate;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    private String studentId;
+}
